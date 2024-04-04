9ca2ae22860fb9b4cd1609cc1eb32a99267a499b
calmjiad <unlucky4246@gmail.com>
2024-04-04 17:38:55 +0200

    blog showcase component responsive completed

--- src/components/BlogShowcase.jsx
+++ src/components/BlogShowcase.jsx
@@ -21,7 +21,7 @@ const items = [
   },
   {
     id: 1,
-    title: "Refs in Vue.js",
+    title: "Understanding Refs in Vue.js",
     description:
       "Vue.js is a popular JavaScript framework for building user interfaces. One of its many powerful features is the ability to reference elements in the...",
     image: "https://i.ibb.co/cyyZQPL/vuejs.jpg",
@@ -42,8 +42,8 @@ const SingleItem = ({ item }) => {
   return (
     <section className="h-[calc(100svh-80px)] snap-start pt-24 ">
       <div className="flex items-center justify-center w-[75%] h-full mx-auto">
-        <div className="max-w-[1366px] h-full m-auto flex gap-12 items-center justify-center overflow-hidden">
-          <div ref={ref} className="h-[50%] w-full">
+        <div className="max-w-[1366px] h-full m-auto flex md:flex-row flex-col gap-12 items-center justify-center overflow-hidden">
+          <div ref={ref} className="lg:h-[50%] md:h-[40%] h-[30%] w-full">
             <img
               className="object-cover w-full h-full"
               src={item.image}
@@ -54,8 +54,12 @@ const SingleItem = ({ item }) => {
             style={{ y }}
             className="flex flex-col gap-3 font-cute-font text-pretty"
           >
-            <h2 className="text-[2.55rem] text-white">{item.title}</h2>
-            <p className="text-[1.4rem]">{item.description}</p>
+            <h2 className="lg:text-[2.55rem] md:text-[1.6rem] text-[1.35rem] md:text-left text-center text-white">
+              {item.title}
+            </h2>
+            <p className="lg:text-[1.4rem] md:text-[1.2rem] text-[0.9rem] md:text-left text-center">
+              {item.description}
+            </p>
             <button className="rounded-md bg-transparent border border-white btn btn-neutral hover:bg-[#2b3344] text-white hover:border-white hover:text-white w-[100%] cursor-pointer text-xl">
               <a href={item.link} target="_blank">
                 Read more
@@ -83,8 +87,13 @@ const BlogShowcase = () => {
       ref={ref}
     >
       <div className="bg-[#0b1221] sticky top-0 left-0 pb-16 pt-20 text-center text-orange-400 text-3xl z-10">
-        <h1 className="pb-4 font-cute-font text-7xl">Recent Articles</h1>
-        <motion.div style={{ scaleX }} className="h-2 bg-white"></motion.div>
+        <h1 className="pb-4 font-cute-font lg:text-7xl text-5xl">
+          Recent Articles
+        </h1>
+        <motion.div
+          style={{ scaleX }}
+          className="md:h-2 h-1 bg-white"
+        ></motion.div>
       </div>
       {items.map((item) => (
         <SingleItem key={item.id} item={item} />
